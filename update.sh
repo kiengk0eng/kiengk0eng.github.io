f512581181d2a0c5f456b19a57e8747d1006a291
Kiengk0eng <kiengk0eng@users.noreply.github.com>
2019-11-26 01:18:48 +0700

    Recovery

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+apt-ftparchive packages ./ > ./Packages
+sed -i -e '/^SHA/d' ./Packages
+bzip2 -c9k ./Packages > ./Packages.bz2
+printf "Origin: Kiengk0eng\nLabel: Kiengk0eng's Repo\nSuite: stable\nVersion: 1.0\nCodename: Kiengk0eng\nArchitecture: iphoneos-arm\nComponents: main\nDescription: Kiengk0eng's Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+exit 0
